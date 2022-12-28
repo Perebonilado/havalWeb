4967f19d63da3f6f923f427567a38ffcd2e99493
Perebonilado <richad.eradiri@cicod.com>
2022-12-28 15:10:12 +0100

    import error fix

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next'
 import React from 'react'
 
-import Auth from "../@modules/auth/auth"
+import Auth from "../@modules/auth/Auth"
 
 const auth:NextPage = () => {
   return (
